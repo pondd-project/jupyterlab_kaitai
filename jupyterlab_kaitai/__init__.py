2837aab448194238fa197e6c7547f9f637036366
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-03-02 09:11:31 -0500

    add example notebooks, rich display

--- jupyterlab_kaitai/__init__.py
+++ jupyterlab_kaitai/__init__.py
@@ -3,7 +3,10 @@ import json
 from pathlib import Path
 
 from ._version import __version__
-from .hexviewer import HexViewer
+from .widget_hex import HexViewer
+from .display_hex import Hex
+
+MIME_TYPE = "application/octet-stream"
 
 HERE = Path(__file__).parent.resolve()
 
